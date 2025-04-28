4bec6b8fb41c1ab10569d6656bf7a85238f30b62
Chunan Ye <chuye@microsoft.com>
2025-04-28 12:50:09 -0700

    Allow CustomChainedCredential to get VS Code credential using an authentication record

--- src/Services/Azure/Authentication/CustomChainedCredential.cs
+++ src/Services/Azure/Authentication/CustomChainedCredential.cs
@@ -4,6 +4,8 @@
 using Azure.Core;
 using Azure.Identity;
 using Azure.Identity.Broker;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace AzureMcp.Services.Azure.Authentication;
 
@@ -18,22 +20,99 @@ namespace AzureMcp.Services.Azure.Authentication;
 /// </remarks>
 public class CustomChainedCredential(string? tenantId = null) : TokenCredential
 {
-    private readonly ChainedTokenCredential _chainedCredential = CreateChainedCredential(tenantId);
+    public override AccessToken GetToken(TokenRequestContext requestContext, CancellationToken cancellationToken)
+    {
+        ChainedTokenCredential chainedCredential = CreateChainedCredentialAsync(tenantId).Result;
+        return chainedCredential.GetToken(requestContext, cancellationToken);
+    }
 
-    public override AccessToken GetToken(TokenRequestContext requestContext, CancellationToken cancellationToken) =>
-        _chainedCredential.GetToken(requestContext, cancellationToken);
+    public override ValueTask<AccessToken> GetTokenAsync(TokenRequestContext requestContext, CancellationToken cancellationToken)
+    {
+        ChainedTokenCredential chainedCredential = CreateChainedCredentialAsync(tenantId).Result;
+        return ValueTask.FromResult(chainedCredential.GetToken(requestContext, cancellationToken));
+    }
 
-    public override ValueTask<AccessToken> GetTokenAsync(TokenRequestContext requestContext, CancellationToken cancellationToken) =>
-        _chainedCredential.GetTokenAsync(requestContext, cancellationToken);
-
-    private static ChainedTokenCredential CreateChainedCredential(string? tenantId) =>
-        new(CreateDefaultCredential(tenantId), CreateBrowserCredential());
-
-    private static InteractiveBrowserCredential CreateBrowserCredential() =>
-        new(new InteractiveBrowserCredentialBrokerOptions(IntPtr.Zero)
+    private static async Task<ChainedTokenCredential> CreateChainedCredentialAsync(string? tenantId)
+    {
+        string? authRecordJson = Environment.GetEnvironmentVariable("AZURE_MCP_AUTH_AUTHENTICATION_RECORD");
+        AuthenticationRecord? authRecord = null;
+        if (!string.IsNullOrEmpty(authRecordJson))
         {
-            UseDefaultBrokerAccount = true
+            byte[] bytes = Encoding.UTF8.GetBytes(authRecordJson);
+            using MemoryStream authRecordStream = new MemoryStream(bytes);
+            authRecord = await AuthenticationRecord.DeserializeAsync(authRecordStream);
+        }
+
+        string? useOnlyBrokerCredential = Environment.GetEnvironmentVariable("AZURE_MCP_USE_ONLY_BROKER_CREDENTIAL");
+        if (useOnlyBrokerCredential == "true")
+        {
+            return new(CreateBrowserCredential(tenantId, authRecord));
+        }
+        else
+        {
+            return new(CreateDefaultCredential(tenantId), CreateBrowserCredential(tenantId, authRecord));
+        }
+    }
+
+    private enum GetAncestorFlags
+    {
+        GetParent = 1,
+        GetRoot = 2,
+        /// <summary>
+        /// Retrieves the owned root window by walking the chain of parent and owner windows returned by GetParent.
+        /// </summary>
+        GetRootOwner = 3
+    }
+
+#if WINDOWS
+    /// <summary>
+    /// Retrieves the handle to the ancestor of the specified window.
+    /// </summary>
+    /// <param name="hwnd">A handle to the window whose ancestor is to be retrieved.
+    /// If this parameter is the desktop window, the function returns NULL. </param>
+    /// <param name="flags">The ancestor to be retrieved.</param>
+    /// <returns>The return value is the handle to the ancestor window.</returns>
+    [DllImport("user32.dll", ExactSpelling = true)]
+    static extern IntPtr GetAncestor(IntPtr hwnd, GetAncestorFlags flags);
+
+    [DllImport("kernel32.dll")]
+    static extern IntPtr GetConsoleWindow();
+
+    public static IntPtr GetConsoleOrTerminalWindow()
+    {
+        IntPtr consoleHandle = GetConsoleWindow();
+        IntPtr handle = GetAncestor(consoleHandle, GetAncestorFlags.GetRootOwner);
+
+        return handle;
+    }
+#else
+   public static IntPtr GetConsoleOrTerminalWindow()
+    {
+        return IntPtr.Zero;
+    } 
+#endif
+
+    private static string TokenCacheName = "azure-mcp-msal.cache";
+
+    private static InteractiveBrowserCredential CreateBrowserCredential(string? tenantId, AuthenticationRecord? authRecord)
+    {
+        string? clientId = Environment.GetEnvironmentVariable("AZURE_MCP_AUTH_CLIENT_ID");
+        string? useOnlyBrokerCredential = Environment.GetEnvironmentVariable("AZURE_MCP_USE_ONLY_BROKER_CREDENTIAL");
+
+        IntPtr handle = GetConsoleOrTerminalWindow();
+
+        return new(new InteractiveBrowserCredentialBrokerOptions(handle)
+        {
+            UseDefaultBrokerAccount = useOnlyBrokerCredential != "true" && authRecord is null,
+            ClientId = clientId,
+            TenantId = tenantId,
+            AuthenticationRecord = authRecord,
+            TokenCachePersistenceOptions = new TokenCachePersistenceOptions()
+            {
+                Name = TokenCacheName,
+            }
         });
+    }
 
     private static DefaultAzureCredential CreateDefaultCredential(string? tenantId)
     {
