250fd9369e3c7936e44849898786da1c116d490d
Connie Yau <conniey@microsoft.com>
2025-06-17 10:37:28 -0700

    Add TelemetryService

---
+++ src/Services/Telemetry/TelemetryConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace AzureMcp.Services.Telemetry
+{
+    internal static class TelemetryConstants
+    {
+        public const string AzureMcpVersion = "Version";
+        public const string SubscriptionGuid = "AzSubscriptionGuid";
+        public const string ResourceHash = "AzResourceHash";
+        public const string ClientName = "ClientName";
+        public const string ClientVersion = "ClientVersion";
+        public const string ToolName = "ToolName";
+    }
+}
