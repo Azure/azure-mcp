67ae15562b055af37c4b0fe47206a0dffa249e50
Connie Yau <conniey@microsoft.com>
2025-07-03 07:13:20 -0700

    Remove unused method.

--- src/Commands/Subscription/SubscriptionCommand.cs
+++ src/Commands/Subscription/SubscriptionCommand.cs
@@ -1,11 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using AzureMcp.Models.Option;
 using AzureMcp.Options;
-using AzureMcp.Services.Telemetry;
 
 namespace AzureMcp.Commands.Subscription;
 
@@ -27,10 +25,5 @@ public abstract class SubscriptionCommand<
         var options = base.BindOptions(parseResult);
         options.Subscription = parseResult.GetValueForOption(_subscriptionOption);
         return options;
-    }
-
-    protected void AddSubscriptionInformation(Activity? activity, TOptions options)
-    {
-        activity?.AddTag(TelemetryConstants.TagName.SubscriptionGuid, options.Subscription);
     }
 }
