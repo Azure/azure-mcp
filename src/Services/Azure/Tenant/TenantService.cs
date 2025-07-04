a1841d2525dabf6b6a6cb0c440f75c6a12334785
Vinay Gera <vigera@microsoft.com>
2025-07-03 23:10:10 -0700

    wire logger

--- src/Services/Azure/Tenant/TenantService.cs
+++ src/Services/Azure/Tenant/TenantService.cs
@@ -7,8 +7,10 @@ using AzureMcp.Services.Caching;
 
 namespace AzureMcp.Services.Azure.Tenant;
 
-public class TenantService(ICacheService cacheService)
-    : BaseAzureService, ITenantService
+using Microsoft.Extensions.Logging;
+
+public class TenantService(ICacheService cacheService, ILoggerFactory loggerFactory)
+    : BaseAzureService(null, loggerFactory), ITenantService
 {
     private readonly ICacheService _cacheService = cacheService ?? throw new ArgumentNullException(nameof(cacheService));
     private const string CacheGroup = "tenant";
