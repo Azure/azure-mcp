ca3bb3f7493914c239cb03c865ca40935fd2e88b
Patrick Hallisey <hallipr@gmail.com>
2025-07-18 10:39:03 -0700

    Update infra/services/monitor-post.ps1

--- infra/services/monitor-post.ps1
+++ infra/services/monitor-post.ps1
@@ -8,6 +8,7 @@ $ErrorActionPreference = "Stop"
 . "$PSScriptRoot/../../eng/common/scripts/common.ps1"
 
 $storageAccountName = "$($BaseName)mon"
+$containerName = 'foo'
 $context = New-AzStorageContext -StorageAccountName $storageAccountName -UseConnectedAccount
 Write-Host "Uploading sample files to blob storage: $storageAccountName/$containerName" -ForegroundColor Yellow
 $files = Get-ChildItem -Path "$PSScriptRoot/../samples" -Filter '*.md'
