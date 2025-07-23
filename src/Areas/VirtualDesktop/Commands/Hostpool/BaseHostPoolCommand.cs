5934f02ae4b2ddb2db6bf89ecc226ee812205c9a
Vladimir Stoyanov (from Dev Box) <vladimis@ntdev.microsoft.com>
2025-07-23 10:25:54 -0700

    reuse resource group option from base class

--- src/Areas/VirtualDesktop/Commands/Hostpool/BaseHostPoolCommand.cs
+++ src/Areas/VirtualDesktop/Commands/Hostpool/BaseHostPoolCommand.cs
@@ -10,19 +10,17 @@ using AzureMcp.Commands.Subscription;
 namespace AzureMcp.Areas.VirtualDesktop.Commands.Hostpool;
 public abstract class BaseHostPoolCommand<
     [DynamicallyAccessedMembers(TrimAnnotations.CommandAnnotations)] T>
-    : SubscriptionCommand<T>
+    : BaseVirtualDesktopCommand<T>
     where T : BaseHostPoolOptions, new()
 {
     protected readonly Option<string> _hostPoolOption = VirtualDesktopOptionDefinitions.HostPool;
     protected readonly Option<string> _hostPoolResourceIdOption = VirtualDesktopOptionDefinitions.HostPoolResourceIdOption;
-    protected readonly Option<string> _hostPoolResourceGroupOption = VirtualDesktopOptionDefinitions.ResourceGroup;
 
     protected override void RegisterOptions(Command command)
     {
         base.RegisterOptions(command);
         command.AddOption(_hostPoolOption);
         command.AddOption(_hostPoolResourceIdOption);
-        command.AddOption(_hostPoolResourceGroupOption);
     }
 
     protected override T BindOptions(ParseResult parseResult)
@@ -30,7 +28,6 @@ public abstract class BaseHostPoolCommand<
         var options = base.BindOptions(parseResult);
         options.HostPoolName = parseResult.GetValueForOption(_hostPoolOption);
         options.HostPoolResourceId = parseResult.GetValueForOption(_hostPoolResourceIdOption);
-        options.ResourceGroup = parseResult.GetValueForOption(_hostPoolResourceGroupOption);
         return options;
     }
 
