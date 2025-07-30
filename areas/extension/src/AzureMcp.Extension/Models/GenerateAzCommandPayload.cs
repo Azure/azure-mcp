d0a6ad36d537a3fa348ad5089e7f3921381fd188
Chunan Ye <chuye@microsoft.com>
2025-07-30 16:34:33 -0700

    Fix json property name

--- areas/extension/src/AzureMcp.Extension/Models/GenerateAzCommandPayload.cs
+++ areas/extension/src/AzureMcp.Extension/Models/GenerateAzCommandPayload.cs
@@ -1,10 +1,14 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.Text.Json.Serialization;
+
 namespace AzureMcp.Extension.Models;
 
 public sealed class GenerateAzCommandPayload
 {
     public string Question { get; set; } = string.Empty;
+
+    [JsonPropertyName("enable_parameter_injection")]
     public bool EnableParameterInjection { get; set; } = true;
 }
