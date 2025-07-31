8e144a1fed065c4cc63d122ab28090eeaa52379c
KarishmaGhiya <kaghiya@microsoft.com>
2025-07-31 13:39:29 -0700

    throw exception

--- areas/servicebus/src/AzureMcp.ServiceBus/Models/utilities/EntityStatusConverter.cs
+++ areas/servicebus/src/AzureMcp.ServiceBus/Models/utilities/EntityStatusConverter.cs
@@ -8,13 +8,13 @@ namespace AzureMcp.ServiceBus.Models.Utilities;
 
 public class EntityStatusConverter : JsonConverter<EntityStatus>
 {
-    public override EntityStatus Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+   public override EntityStatus Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         if (reader.TokenType == JsonTokenType.String)
         {
             return new EntityStatus(reader.GetString());
         }
-        return default;
+        throw new JsonException($"Unexpected token type {reader.TokenType} for EntityStatus.");
     }
 
     public override void Write(Utf8JsonWriter writer, EntityStatus value, JsonSerializerOptions options)
