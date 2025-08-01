e03b74a160054c0d35a5328cd86458f0fd6429cd
KarishmaGhiya <kaghiya@microsoft.com>
2025-08-01 15:42:48 -0700

    update

---
+++ areas/appservice/src/AzureMcp.AppService/Options/BaseAppServiceOptions.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using AzureMcp.Core.Options;
+using System.Text.Json.Serialization;
+
+namespace AzureMcp.AppService.Options;
+
+public class BaseAppServiceOptions : SubscriptionOptions
+{
+    [JsonPropertyName(AppServiceOptionDefinitions.AppName)]
+    public string? AppName { get; set; }
+}
