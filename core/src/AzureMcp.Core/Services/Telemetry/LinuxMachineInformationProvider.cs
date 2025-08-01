7d3f70fd92cdc8ad7c9fc67e455783284310e205
Connie Yau <conniey@microsoft.com>
2025-08-01 04:39:52 -0700

    Add information providers to get device id and mac address

---
+++ core/src/AzureMcp.Core/Services/Telemetry/LinuxMachineInformationProvider.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Runtime.Versioning;
+using Microsoft.Extensions.Logging;
+
+namespace AzureMcp.Core.Services.Telemetry;
+
+[SupportedOSPlatform("linux")]
+
+internal class LinuxMachineInformationProvider : UnixInformationProvider
+{
+    private const string PrimaryPathEnvVar = "XDG_CACHE_HOME";
+    private const string SecondaryPathEnvVar = "HOME";
+    private const string SecondaryPathSubDirectory = ".cache";
+
+    private readonly ILogger<LinuxMachineInformationProvider> _logger;
+
+    public LinuxMachineInformationProvider(ILogger<LinuxMachineInformationProvider> logger)
+        : base(logger)
+    {
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Gets the base folder for the cache to be stored.
+    /// The final path should be $HOME\.cache\Microsoft\DeveloperTools.
+    /// </summary>
+    protected override string GetStoragePath()
+    {
+        var userDir = Environment.GetEnvironmentVariable(PrimaryPathEnvVar);
+
+        // If this comes back as null or empty/whitespace, try environment variable.
+        if (string.IsNullOrWhiteSpace(userDir))
+        {
+            var rootPath = Environment.GetEnvironmentVariable(SecondaryPathEnvVar);
+
+            // If the secondary path is still null/empty/whitespace, then throw as it will lead
+            // to us caching the data in the wrong directory otherwise.
+            if (string.IsNullOrWhiteSpace(rootPath))
+            {
+                throw new InvalidOperationException("linux: Unable to get UserProfile or $HOME folder.");
+            }
+
+            userDir = Path.Combine(rootPath, SecondaryPathSubDirectory);
+        }
+
+        return userDir;
+    }
+}
