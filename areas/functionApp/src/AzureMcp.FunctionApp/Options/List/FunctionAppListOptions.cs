bf5d2532ae214a9bd83250ebf1524ac3ea591d9e
Colby Timm <timmcolby@gmail.com>
2025-08-07 19:06:34 -0700

    Remove duplicate

--- areas/functionApp/src/AzureMcp.FunctionApp/Options/List/FunctionAppListOptions.cs
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-namespace AzureMcp.FunctionApp.Options;
-
-public class FunctionAppListOptions : BaseFunctionAppOptions;
