facc84907d5848cfcec805ee2fe88f88fa945471
Colby Timm <timmcolby@gmail.com>
2025-08-12 00:07:31 -0700

    Refactor BaseFunctionAppCommand to use SubscriptionOptions and update FunctionAppListOptions to include necessary using directive.

--- areas/functionapp/src/AzureMcp.FunctionApp/Commands/BaseFunctionAppCommand.cs
+++ areas/functionapp/src/AzureMcp.FunctionApp/Commands/BaseFunctionAppCommand.cs
@@ -4,10 +4,10 @@
 using System.Diagnostics.CodeAnalysis;
 using AzureMcp.Core.Commands;
 using AzureMcp.Core.Commands.Subscription;
-using AzureMcp.FunctionApp.Options;
+using AzureMcp.Core.Options;
 
 namespace AzureMcp.FunctionApp.Commands;
 
 public abstract class BaseFunctionAppCommand<
     [DynamicallyAccessedMembers(TrimAnnotations.CommandAnnotations)] TOptions>
-    : SubscriptionCommand<TOptions> where TOptions : BaseFunctionAppOptions, new();
+    : SubscriptionCommand<TOptions> where TOptions : SubscriptionOptions, new();
