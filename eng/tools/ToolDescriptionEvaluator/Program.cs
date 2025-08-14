3cff9ab42acaf694596fe56ddd2879cdb52b7b9f
vcolin7 <victor.y.asi@gmail.com>
2025-08-14 14:43:06 -0700

    Added licensing header to .cs files

--- eng/tools/ToolDescriptionEvaluator/Program.cs
+++ eng/tools/ToolDescriptionEvaluator/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Diagnostics;
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Diagnostics;
 using System.Text.Json;
 using ToolSelection.Models;
 using ToolSelection.Services;
