d6f4c418c922268aac15c6ed8d03fe76d848bdf8
vcolin7 <victor.y.asi@gmail.com>
2025-08-13 22:02:11 -0700

    Removed unused imports

--- areas/keyvault/tests/AzureMcp.KeyVault.UnitTests/Certificate/CertificateImportCommandTests.cs
+++ areas/keyvault/tests/AzureMcp.KeyVault.UnitTests/Certificate/CertificateImportCommandTests.cs
@@ -2,8 +2,6 @@
 // Licensed under the MIT License.
 
 using System.CommandLine.Parsing;
-using System.Collections.Generic;
-using System;
 using AzureMcp.Core.Models.Command;
 using AzureMcp.Core.Options;
 using AzureMcp.KeyVault.Commands.Certificate;
