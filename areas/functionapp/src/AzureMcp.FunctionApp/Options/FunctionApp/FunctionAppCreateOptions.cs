bb5fbfb6cab09a9b9e5543485737ec3b25688bba
Colby Timm <timmcolby@gmail.com>
2025-08-14 20:16:50 -0700

    Add FunctionAppCreateOptions class for function app creation configuration

---
+++ areas/functionapp/src/AzureMcp.FunctionApp/Options/FunctionApp/FunctionAppCreateOptions.cs
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Text.Json.Serialization;
+using AzureMcp.Core.Options;
+
+namespace AzureMcp.FunctionApp.Options.FunctionApp;
+
+public class FunctionAppCreateOptions : BaseFunctionAppOptions
+{
+    [JsonPropertyName(FunctionAppOptionDefinitions.LocationName)]
+    public string? Location { get; set; }
+
+    [JsonPropertyName(FunctionAppOptionDefinitions.AppServicePlanName)]
+    public string? AppServicePlan { get; set; }
+
+    [JsonPropertyName(FunctionAppOptionDefinitions.PlanTypeName)]
+    public string? PlanType { get; set; }
+
+    [JsonPropertyName(FunctionAppOptionDefinitions.StorageConnectionStringName)]
+    public string? StorageConnectionString { get; set; }
+
+    [JsonPropertyName(FunctionAppOptionDefinitions.RuntimeName)]
+    public string? Runtime { get; set; }
+
+    [JsonPropertyName(FunctionAppOptionDefinitions.RuntimeVersionName)]
+    public string? RuntimeVersion { get; set; }
+}
