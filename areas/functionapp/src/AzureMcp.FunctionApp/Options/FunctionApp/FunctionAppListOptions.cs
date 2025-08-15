c0f2d0798f83d780139760e5ca0cded0d2a7c85b
Colby Timm <timmcolby@gmail.com>
2025-08-14 20:16:05 -0700

    Refactor FunctionAppListCommand to remove unused telemetry service import and move FunctionAppListOptions class

--- areas/functionapp/src/AzureMcp.FunctionApp/Options/FunctionApp/FunctionAppListOptions.cs
+++ areas/functionapp/src/AzureMcp.FunctionApp/Options/FunctionApp/FunctionAppListOptions.cs
@@ -3,6 +3,6 @@
 
 using AzureMcp.Core.Options;
 
-namespace AzureMcp.FunctionApp.Options;
+namespace AzureMcp.FunctionApp.Options.FunctionApp;
 
 public class FunctionAppListOptions : SubscriptionOptions;
