175e4a4f1a4869a3ce1c3654c0ca6ff8109676db
Colby Timm <timmcolby@gmail.com>
2025-08-14 20:17:25 -0700

    Add global using directives for System.CommandLine and AzureMcp.Core.Options

--- areas/functionapp/src/AzureMcp.FunctionApp/GlobalUsings.cs
+++ areas/functionapp/src/AzureMcp.FunctionApp/GlobalUsings.cs
@@ -2,4 +2,6 @@
 // Licensed under the MIT License.
 
 global using System.CommandLine.Parsing;
+global using System.CommandLine;
+global using AzureMcp.Core.Options;
 global using AzureMcp.Core.Models.Command;
